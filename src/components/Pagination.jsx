36520a9ef57eb31401c8a894ea7572dfd090407d
AshwekPadolkar <padolkarashwek11@gmail.com>
2024-10-07 11:13:10 +0530

    pagination controls optimized

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -3,7 +3,7 @@ import { PaginationContext } from "../store/paginationStore";
 
 const Pagination = () => {
   const { state, dispatchData } = useContext(PaginationContext);
-  const { currentPage } = state;
+  const { currentPage, isLoading } = state;
 
   const handlePageChange = (page) => {
     if (page >= 1 && page !== currentPage) {
@@ -16,31 +16,35 @@ const Pagination = () => {
 
   return (
     <div className="pagination-controls">
-      <button
-        onClick={() => handlePageChange(currentPage - 1)}
-        disabled={currentPage === 1}
-        className="arrow-button"
-      >
-        &#10094;
-      </button>
-      {Array.from({ length: 4 }, (item, index) => {
-        const startPage = Math.max(currentPage - 2, 1);
-        return startPage + index;
-      }).map((page) => (
+      <div className="controls">
         <button
-          key={page}
-          onClick={() => handlePageChange(page)}
-          className={`page-number ${page === currentPage ? "current" : ""}`}
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1 || isLoading}
+          className="arrow-button"
         >
-          {page}
+          &#10094;
         </button>
-      ))}
-      <button
-        onClick={() => handlePageChange(currentPage + 1)}
-        className="arrow-button"
-      >
-        &#10095;
-      </button>
+        {Array.from({ length: 4 }, (item, index) => {
+          const startPage = Math.max(currentPage - 2, 1);
+          return startPage + index;
+        }).map((page) => (
+          <button
+            key={page}
+            onClick={() => handlePageChange(page)}
+            disabled={isLoading}
+            className={`page-number ${page === currentPage ? "current" : ""}`}
+          >
+            {page}
+          </button>
+        ))}
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={isLoading}
+          className="arrow-button"
+        >
+          &#10095;
+        </button>
+      </div>
     </div>
   );
 };
